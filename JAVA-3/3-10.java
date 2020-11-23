3b2d4d11104dd77198ea984a8babde48d8148a16
Roman <65686226+morozit@users.noreply.github.com>
2020-11-23 16:03:15 +0200

    Create 3-10.java

---
+++ JAVA-3/3-10.java
@@ -0,0 +1,11 @@
+public class ChatBot {
+    public static int validateMonthCount(int monthCount) {
+       //Пишите код под этой строкой
+      
+       if (monthCount == 0) {
+            return 1;
+        } else {
+            return monthCount;
+        }
+    }
+}
