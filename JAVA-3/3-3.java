5d16b1ba8449635ebe5ed91c4faf14df31e814c9
Roman <65686226+morozit@users.noreply.github.com>
2020-11-23 16:03:27 +0200

    Create 3-3.java

---
+++ JAVA-3/3-3.java
@@ -0,0 +1,9 @@
+public class ChatBot {
+    public static int findMaxNumber(int a, int b) {
+        if (a > b) {
+            return a; //Пишите код в этой строке
+        } else {
+            return b; //Пишите код в этой строке
+        }
+    }
+}
