106b262b7f6ddfa9f9c6d50c4763954cf679bb1a
Roman <65686226+morozit@users.noreply.github.com>
2020-11-23 16:03:24 +0200

    Create 3-2.java

---
+++ JAVA-3/3-2.java
@@ -0,0 +1,11 @@
+public class ChatBot {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+ 
+        //Пишите код под этой строкой
+        boolean eq = a == b;
+        boolean lessThan = a < b;
+        boolean greaterThan = a > b; 
+    }
+}
