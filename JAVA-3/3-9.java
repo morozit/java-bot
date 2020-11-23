6e8871553d9304b09baeb14bdd7d3a93faefb26a
Roman <65686226+morozit@users.noreply.github.com>
2020-11-23 16:03:43 +0200

    Create 3-9.java

---
+++ JAVA-3/3-9.java
@@ -0,0 +1,7 @@
+
+public class ChatBot {
+    public static int calculateMonthCount(int dreamCost, int professionSalary) {
+        //Пишите код под этой строкой
+        return dreamCost / professionSalary;
+    }
+}
